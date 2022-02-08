9a96c17e3d5e257ee02dc68ea82ffdd24bd4ce18
eks1990 <96073733+eks1990@users.noreply.github.com>
2022-02-08 07:52:08 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+x=[1,'A',3.4,1,'A']
+print(x[0])
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+sum= x[0]+x[2]
+print("sum is:", sum )
+print(type(x))
+print(type(x[0]))
+print(type(x[1]))
+print(type(x[2]))
